3bbbfaf4f6ec10cb4e825fa7c9b248f938ec63ef
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-02-08 01:46:26 +0000

    Made hello world in Swift.

---
+++ HelloWorld.swift
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Sarah Andrew
+//  Created on 2023-02-07
+//  Version 1.0
+//  Copyright (c) 2023 Sarah. All rights reserved.
+//
+//  This program executes hello world.
+
+// print following statements
+print("Hello!")
+print("We love programming!")
